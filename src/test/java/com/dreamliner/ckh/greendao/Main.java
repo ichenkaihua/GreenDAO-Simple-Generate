52f2df7752c3c991b2597b20328a0fe8969a19c5
=ichenkaihua <=admin@chenkaihua.com>
2015-08-12 23:30:18 +0800

    修改测试代码

--- src/test/java/com/dreamliner/ckh/greendao/Main.java
+++ src/test/java/com/dreamliner/ckh/greendao/Main.java
@@ -1,23 +1,3 @@
-/**   
-
- * Copyright © 2015 Dreamliner Studio. All rights reserved.
- 
- * 
-
- * @Title: Main.java
- 
- * @Prject: GreenDao2.0
- 
- * @Package: com.dreamliner.ckh.greendao
- 
- * @author: chenkaihua  
- 
- * @date: 2015年6月25日 下午4:40:21
- 
- * @version: V1.0  
- 
- */
-
 package com.dreamliner.ckh.greendao;
 
 import com.github.ichenkaihua.greendao.annotation.EntityInject;
@@ -30,10 +10,13 @@ import com.github.ichenkaihua.greendao.service.GenerateService;
 import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
-@GenerateConfig(outDir = "/home/chenkaihua/workspace/QueryScore/src/main/java",
-	schemaConfig = @SchemaConfig(defaultJavaPackage = "com.dreamlienr.queryscore.entity"
-	,defaultJavaPackageDao="com.dreamlienr.queryscore.dao")
-)
+//GenerateConfig.outDir: 输出文件夹路径，必须存在这个路径，为避免出错，建议路径分隔符用 /
+//这个路径可以是绝对路径，/home/chenkaihua/workspace/QueryScore/src/main/java
+//也可以是相对路径，比如在同一个workspace的android项目名为 MyAndroidProject,则可以写成 ../MyAndroidProject/src
+//特别注意的是，如果是android studio项目，应该这样写 app/src/main/java
+// GenerateConfig.SchemaConfig.defaultJavaPackage 生成的entity的所在包，如果没有包名或包名不完全，则greendao会自动创建
+//GenerateConfig.SchemaConfig.defaultJavaPackageDao 生成的dao类所在包，如果没有包名或包名不完全，则greendao会自动创建
+@GenerateConfig(outDir = "/home/chenkaihua/workspace/QueryScore/src/main/java", schemaConfig = @SchemaConfig(defaultJavaPackage = "com.dreamlienr.queryscore.entity", defaultJavaPackageDao = "com.dreamlienr.queryscore.dao"))
 public class Main {
 
 	// 通过EntityInject注解，可以定义Entity的名字
@@ -79,7 +62,12 @@ public class Main {
 
 	}
 
+	// Main方法
 	public static void main(String[] args) {
+		// 传入带有类注解的Main.class类，GreenDao-Simple-Generate将会扫描类注解，配置输出路径，输出包名
+		// 然后扫描这个类下的所有（public protected甚至private）方法
+		// 如果方法参数有Schema、GenerateInfo、SchemaInfo、@EntityInject注解，则系统会注入相应对象
+		// 最后调用这个方法
 		new GenerateService(Main.class).generate();
 	}
 
